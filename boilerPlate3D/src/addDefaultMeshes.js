3e771b2fe5769b5002b3b8699a22ec5e999d7211
Brian Ho <bh1525@nyu.edu>
2025-02-12 20:41:01 -0600

    added mesh physical

--- boilerPlate3D/src/addDefaultMeshes.js
+++ boilerPlate3D/src/addDefaultMeshes.js
@@ -4,7 +4,36 @@ import {
 	MeshBasicMaterial,
 	MeshStandardMaterial,
 	Mesh,
+	TextureLoader,
+	MeshPhysicalMaterial,
+	SphereGeometry,
 } from 'three'
+
+//define my texture loader
+const loader = new TextureLoader()
+
+//example function using textures and mesh physical material
+export const addTexturedMesh = () => {
+	// color === map
+	const color = loader.load('Ice_001_COLOR.jpg')
+	const normal = loader.load('Ice_001_NRM.jpg')
+	const displace = loader.load('Ice_001_DISP.png')
+	const ambientOcclusion = loader.load('Ice_001_OCC.jpg')
+	const sphere = new SphereGeometry(0.75, 100, 100)
+	const sphereMaterial = new MeshPhysicalMaterial({
+		map: color,
+		normalMap: normal,
+		displacementMap: displace,
+		displacementScale: 0.3,
+		aoMap: ambientOcclusion,
+		metalness: 0.1,
+		roughness: 0,
+		transmission: 0.5,
+		ior: 2.33,
+	})
+	const sphereMesh = new Mesh(sphere, sphereMaterial)
+	return sphereMesh
+}
 
 //we're creating two simple functions now that create two different types of boxes, one made of a mesh basic material, one made of a mesh standard material and each is given a different color
 export const addBoilerPlateMeshes = () => {
